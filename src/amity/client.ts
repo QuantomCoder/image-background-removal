c0b1d1506610e29097c3b9c28fa98b9a9529fb6d
Muhammad Umar <146330025+QuantomCoder@users.noreply.github.com>
2024-03-12 12:08:35 +0500

    Update client.ts

--- src/amity/client.ts
+++ src/amity/client.ts
@@ -8,7 +8,7 @@ let amity = async () => {
       }
     }
 
-    const client: Amity.Client = Client.createClient("b0e9bc0f3ddff2341d3e8f4d560a1fdad800deb1ec663e25", API_REGIONS.US);
+    const client: Amity.Client = Client.createClient("", API_REGIONS.US);
     console.log("client login",sessionHandler)
     let isConnected = await client.mqtt.connect({ userId: "umar", sessionHandler })
     console.log(isConnected)
